90284c9cef5fc8f8fb7c82fa4c4e670cde03099e
hhy <huanghaiyang@shuxin-tech.com>
2025-09-15 11:11:01 +0800

    utils

---
+++ packages/utils/src/test.ts
@@ -0,0 +1,20 @@
+import NpmPackage from './NpmPackage.js';
+import { getLatestVersion, getNpmInfo,  getNpmRegistry, getVersions } from './versionUtils.js';
+import path from 'node:path';
+
+async function main() {
+    const pkg = new NpmPackage({
+        targetPath: path.join(import.meta.dirname, '../aaa'),
+        name: 'create-vite'
+    });
+
+    if(await pkg.exists()) {
+        pkg.update();
+    } else {
+        pkg.install();
+    }
+
+    console.log(await pkg.getPackageJSON())
+}
+
+main();
